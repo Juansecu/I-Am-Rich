3566780d859844b6446d03835dc82d4a8ce19e09
Juansecu <juansequ@hotmail.com>
2020-11-15 02:28:55 -0500

    First commit.

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    // For more information about MaterialApp, you can visit the link below:
+    // https://material.io/
+    MaterialApp(
+      // For more information about Scaffold, you can visit the link below:
+      // https://api.flutter.dev/flutter/material/Scaffold-class.html
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[800],
+        appBar: AppBar(
+          title: Text('I Am Rich!'),
+          // For more information about the color system of MaterialApp,
+          // you can visit the link below:
+          // https://material.io/design/color/the-color-system.html
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image:
+                NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
